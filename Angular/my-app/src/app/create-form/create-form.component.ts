a4b2400afae2677e488cbfa5105ac5db48d1f7fa
Jamarius Epperson <jayepperson@yahoo.com>
2018-09-28 17:02:20 -0400

    adding new case finished

--- Angular/my-app/src/app/create-form/create-form.component.ts
+++ Angular/my-app/src/app/create-form/create-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SocialwService } from './socialw.service';
 import { PlacementService } from './placement.service';
+import { SubmitService } from './submit.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-form',
@@ -11,7 +13,9 @@ export class CreateFormComponent implements OnInit {
 
   constructor(
     private socialw:SocialwService,
-    private placement:PlacementService
+    private placement:PlacementService,
+    private submit:SubmitService,
+    private router: Router
   ) { }
 
   thisSoc = null;
@@ -25,6 +29,28 @@ export class CreateFormComponent implements OnInit {
     this.placement.getPlacement().subscribe(data =>{
       this.thisp = data;
       console.log(data);
+    })
+  }
+
+  newForm(event){
+    event.preventDefault()
+    const target = event.target
+    const firstname = target.querySelector('#firstname').value
+    const lastname = target.querySelector('#lastname').value
+    const birthdate = target.querySelector('#birthdate').value
+    const rating = target.querySelector('#rating').value
+    const socialw = target.querySelector('#socialworker').value
+    const placement = target.querySelector('#placement').value
+    console.log(firstname,lastname,birthdate,rating,socialw,placement);
+    
+    this.submit.submitForm(firstname,lastname,birthdate,rating,socialw,placement).subscribe(data =>{
+      if(data){
+        window.alert("Case Saved");
+        this.router.navigate(['dashboard'])
+      }
+      else{
+        window.alert("Could not save case")
+      }
     })
   }
 }
